6159cc09f3e56a6abc85fee22c322fcb45a72336
Shaffay Bajwa <33603579+shaffay@users.noreply.github.com>
2022-05-11 18:39:27 +0500

    Initial commit

---
+++ session1.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Shaffay Bajwa";
+
+$a = 10;
+
+$b = 20;
+
+// echo "Hello ".$name;
+$b = $a+$b;
+
+$a = 20;
+$c = $a - $b;
+$a = $b;
+$z = $c;
+$c = $z + $a + $b;
+echo $a + $b + $z - $c;
+
+?>
